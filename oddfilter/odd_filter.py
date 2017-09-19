230dee3554fb7f7702be3269a095b234634a2ddd
Attila Korom <attilakorom2014@gmail.com>
2017-09-19 09:21:26 +0200

    feat(odd_filter.py): function skeleton

--- oddfilter/odd_filter.py
+++ oddfilter/odd_filter.py
@@ -2,4 +2,11 @@
 # and returns a new list with every odd number from the orignal list
 # example: [1, 2, 3, 4, 5] should produce [1, 3, 5]
 
-print(odd_filter([1, 2, 3, 4, 5]))  # should print [1, 3, 5]
+odd_filter = [1, 2, 3, 4, 5]  # should print [1, 3, 5]
+
+def odd_number_filter_func(number_list):
+    filtered_list = []   
+    print(filtered_list)
+
+
+odd_number_filter_func(odd_filter)
